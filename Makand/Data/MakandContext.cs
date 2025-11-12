7721ce66713b6290703f893236fad7b85ec56ee9
kron0zzz <alanandrey234@gmail.com>
2025-11-12 09:49:30 -0500

    Estructura basica

---
+++ Makand/Data/MakandContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Makand.Models;
+
+namespace Makand.Data
+{
+    public class MakandContext: DbContext
+    {
+        public MakandContext(DbContextOptions<MakandContext> options) : base(options) { }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ Makand/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace Makand.Models
+{
+    public class Cliente
+    {
+        public int Nro_documento { get; set; }
+        public int Tipo_documento { get; set; }
+        public string Nombre_completo { get; set; }
+        public int Numero_telefono { get; set; }
+        public string Email {  get; set; }
+        public string Direccion {  get; set; }
+        public int Tipo_organizacion { get; set; }
+
+    }
+}
